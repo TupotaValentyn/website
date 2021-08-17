ac76d2fa429a72ab29c64cb381e87800b4674142
winderton <richard.hendrix.art@gmail.com>
2021-08-18 02:54:42 +0300

    https server

--- backend/server.js
+++ backend/server.js
@@ -1,38 +1,42 @@
 const express = require('express')
 const next = require('next')
+const fs = require('fs');
+const https = require('https');
 
-/**
- const path = require('path')
- */
+const cert = fs.readFileSync('./backend/ssl/wndtn_com.crt');
+const ca = fs.readFileSync('./backend/ssl/wndtn_com.ca-bundle');
+const key = fs.readFileSync('./backend/ssl/wndtn_com.key');
 
 const server = express();
 
-const port = parseInt(process.env.PORT, 10) || 3000
-const dev = process.env.NODE_ENV !== 'production'
-const app = next({ dev })
-const handle = app.getRequestHandler()
+const port = parseInt(process.env.PORT, 10) || 3000;
+const dev = process.env.NODE_ENV !== 'production';
+const app = next({ dev });
+const handle = app.getRequestHandler();
+
+const hostname = 'wndtn.com';
+const httpsport = 443;
+
+let options = {
+  cert: cert, 
+  ca: ca, 
+  key: key
+};
 
 
+const httpsServer = https.createServer(options, server);
 
 app.prepare().then(() => {
 
-  /*
-  server.use(express.static(path.join(__dirname, '../out')));
-
-  server.get('/', (req,res) => {
-    res.sendFile(path.join(__dirname, '../out/index.html'));
-  });
-  */
-
-  server.get('/api/show', (req, res) => {
+  httpsServer.get('/api/show', (req, res) => {
     return res.end("rendered on the server");
   })
 
-  server.all('*', (req, res) => {
+  httpsServer.all('*', (req, res) => {
     return handle(req, res)
   })
 
-  server.listen(port, (err) => {
-    if (err) throw err
-  })
+  httpsServer.listen(httpsport, hostname);
 })
+
+ 
